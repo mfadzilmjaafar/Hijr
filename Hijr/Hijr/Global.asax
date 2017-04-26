51872b647c681ef4bf4a9ba4f4b42d347ed07ac4
mfadzil <mfadzilmjaafar@yahoo.com>
2017-04-26 11:49:45 +0800

    Test Commit

---
+++ Hijr/Hijr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hijr.MvcApplication" Language="C#" %>
---
+++ Hijr/Hijr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hijr.Startup))]
+namespace Hijr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
